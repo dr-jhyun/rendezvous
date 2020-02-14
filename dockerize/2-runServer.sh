59df6c6866c0dea9782cfcc10386bf4a858c9c4d
Jihyuck Yun <dr.jhyun@gmail.com>
2020-02-14 10:35:44 +0900

    Fix docker run file

--- dockerize/2-runServer.sh
+++ dockerize/2-runServer.sh
@@ -1,1 +1,1 @@
-docker run --network=host --rm --volume=${PWD}/..:/root --volume=/var/run/docker.sock:/var/run/docker.sock rendezvous rendezvous -f config-server.yaml
+docker run --network=host --rm --detach --volume=${PWD}/..:/root --volume=/var/run/docker.sock:/var/run/docker.sock rendezvous rendezvous -f config-server.yaml
